f35484d711943d2995fefcf8595034623ef9e315
zengzhen2 <928930375@qq.com>
2016-09-05 20:19:43 +0800

    20160905项目创建

---
+++ app/src/main/java/com/android1604/mustsee/BaseActivity.java
@@ -0,0 +1,16 @@
+package com.android1604.mustsee;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by zengzhen on 2016/9/5.
+ *
+ */
+public class BaseActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
---
+++ app/src/main/java/com/android1604/mustsee/MyApplication.java
@@ -0,0 +1,13 @@
+package com.android1604.mustsee;
+
+import android.app.Application;
+
+/**
+ * Created by Administrator on 2016/9/5.
+ */
+public class MyApplication extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
