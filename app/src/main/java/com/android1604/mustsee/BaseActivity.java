776d0afb617b4ed9c20a4ef45866e99ff8972221
zengzhen2 <928930375@qq.com>
2016-09-06 11:24:16 +0800

    20160906  增加友盟统计

--- app/src/main/java/com/android1604/mustsee/BaseActivity.java
+++ app/src/main/java/com/android1604/mustsee/BaseActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 
+import com.umeng.analytics.MobclickAgent;
+
 /**
  * Created by zengzhen on 2016/9/5.
  *
@@ -12,5 +14,19 @@ public class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        //友盟统计
+        MobclickAgent.onResume(this);
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //友盟统计
+        MobclickAgent.onPause(this);
     }
 }
